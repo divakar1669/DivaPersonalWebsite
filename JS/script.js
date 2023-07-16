0c296bf83939cb49d423a43a8a1c6464d7e348d5
divakar.tk <divakar.tk@zohocorp.com>
2023-07-16 12:02:24 +0530

    primer done

---
+++ JS/script.js
@@ -0,0 +1,51 @@
+var i = 0;
+var txt = ` Hey, I am Divakar Thiyagarajen, an inquisitive tech enthusiast, constantly seeking to expand my
+knowledge and grasp new technologies. I strive to maintain a foundational understanding of how
+things operate. This webpage aims to provide a brief overview of my educational and professional
+journey thus far, offering a glimpse into my experiences.`;
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("aboutDiva").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function darkMode()
+{
+    var checkBox = document.getElementById("toggle");
+    const root = document.querySelector(':root');
+    const image = document.getElementById("moon_Or_Sun");
+    if(checkBox.checked) // DARK
+    {
+        image.classList.remove("icon--light");
+        image.classList.add("icon--dark");
+        image.name="moon-outline";
+        root.style.setProperty('--text-color', 'white');
+        root.style.setProperty('--backrgoud-color', 'black');
+    }
+    else //LIGHT
+    {
+        image.classList.remove("icon--dark");
+        image.classList.add("icon--light");
+        image.name="sunny-outline";
+        root.style.setProperty('--text-color', 'black');
+        root.style.setProperty('--backrgoud-color', 'white');
+    }
+
+}
+
+function scrollToElement(elementId) {
+    var headerHeight = document.querySelector('.fixed-header').offsetHeight;
+  var element = document.getElementById(elementId);
+  var offsetTop = element.offsetTop - headerHeight;
+
+  window.scrollTo({
+    top: offsetTop,
+    behavior: 'smooth'
+  });
+}
+
+typeWriter();
